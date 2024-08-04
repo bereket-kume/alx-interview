97b6ef1cabf5717610e73df26721612d903b51b2
bereket-kume <bereketkume@gmail.com>
2024-08-04 18:05:34 +0300

    done

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def canUnlockAll(boxes):
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+    keys = list(boxes[0])
+
+    while keys:
+        current_key = keys.pop()
+        if current_key < n and not unlocked[current_key]:
+            unlocked[current_key] = True
+            for key in boxes[current_key]:
+                if key < n and not unlocked[key]:
+                    keys.append(key)
+
+    return all(unlocked)

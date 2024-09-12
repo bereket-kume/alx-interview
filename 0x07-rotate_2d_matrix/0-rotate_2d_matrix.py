552612e74a8d5fdfee4dbb1297ffca21e213fe9f
bereket-kume <bereketkume@gmail.com>
2024-09-13 00:39:25 +0300

    rotate 2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def rotate_2d_matrix(matrix):
+    """
+    function rotate 2d matrix
+    """
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for row in matrix:
+        row.reverse()

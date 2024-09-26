9150852304d1e1bf92bcc9fdd515206f83d8fadf
bereket-kume <bereketkume@gmail.com>
2024-09-26 21:57:34 +0300

    fix requirement error

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,6 +3,7 @@
 Island perimeter
 """
 
+
 def island_perimeter(grid):
     """
     function to calculate perimeter
@@ -15,15 +16,15 @@ def island_perimeter(grid):
     for i in range(row):
         for j in range(column):
             if grid[i][j] == 1:
-                if i == 0 or grid[i-1][j] == 0:
+                if i == 0 or grid[i - 1][j] == 0:
                     count += 1
-                
+
                 if (row - i) == 1 or grid[i + 1][j] == 0:
                     count += 1
-                
+
                 if j == 0 or grid[i][j - 1] == 0:
                     count += 1
-                
+
                 if (column - j) == 1 or grid[i][j + 1] == 0:
                     count += 1
 
